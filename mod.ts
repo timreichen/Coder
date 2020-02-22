87f4c8c637aee6529aa0e78ca06bceb018ebad5a
Tim Reichen <tim.reichen@icloud.com>
2020-02-22 12:08:47 +0100

    make defaultDataTypes importable

--- mod.ts
+++ mod.ts
@@ -15,7 +15,6 @@ import { InfinitydDataType } from "./datatypes/Infinity.ts"
 import { Set16DataType, Set32DataType, Set8DataType } from "./datatypes/Set.ts"
 import { Map16DataType, Map32DataType, Map8DataType } from "./datatypes/Map.ts"
 
-
 export { DataType, MultiDataType } from "./DataType.ts"
 import { Coder } from "./Coder.ts"
 export { Encoder } from "./Encoder.ts"
@@ -24,7 +23,7 @@ export { Decoder } from "./Decoder.ts"
 export const coder = new Coder()
 export { Coder }
 
-const dataTypes = [
+export const defaultDataTypes = [
   new FixedUintDataType([
     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
     0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
@@ -108,4 +107,4 @@ const dataTypes = [
   
 ]
 
-coder.register(...dataTypes)
+coder.register(...defaultDataTypes)
