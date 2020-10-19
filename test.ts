ccb5d1af109e8eb2b03821dfbe9c55a21646bb5e
Tim Reichen <tim.reichen@gmail.com>
2020-10-20 00:12:59 +0200

    update deps

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.60.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts";
 import { coder } from "./mod.ts";
 
 Deno.test("null", () => {
